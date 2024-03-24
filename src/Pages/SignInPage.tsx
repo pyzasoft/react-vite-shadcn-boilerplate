8a0958147147eaf0fa719ee47e93f3c9c2924818
Ashekur Rahman <md.ashekur@gmail.com>
2024-03-24 07:37:31 +0600

    refactor content

--- src/Pages/SignInPage.tsx
+++ src/Pages/SignInPage.tsx
@@ -32,15 +32,14 @@ export const SignInPage = () => {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            Ecom
+            Project
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
               <p className="text-lg">
-                &ldquo;This has saved me countless hours of work and helped me
-                deliver products my customers faster than ever before.&rdquo;
+                &ldquo;This has saved me countless hours of work.&rdquo;
               </p>
-              <footer className="text-sm">Sofia Davis</footer>
+              <footer className="text-sm">Mark Sinista</footer>
             </blockquote>
           </div>
         </div>
