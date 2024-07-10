04a9a0c7d4ac14f5ad2f30c712afcd658dcb0a73
Ashekur Rahman <md.ashekur@gmail.com>
2024-07-11 02:41:08 +0600

    Update index.ts

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-export const rootReducer = () => combineReducers({});
+export const rootReducer = combineReducers({});
 
 export default rootReducer;
